5ea8fbc73131df1d23289652d1adba5990c06822
huangshaowen01 <huangshaowen01@baidu.com>
2017-09-08 15:16:16 +0800

    调整格式
    Change-Id: I47d25cd32da64f4dc87988536005ff95bd7d6e1d

--- src/com/baidu/trace/util/TimeUtils.java
+++ src/com/baidu/trace/util/TimeUtils.java
@@ -8,17 +8,17 @@ package com.baidu.trace.util;
  */
 public class TimeUtils {
 
-	public static final int MILLIS_TO_SECONDS = 1000;
+    public static final int MILLIS_TO_SECONDS = 1000;
 
-	public static final int MILLIS_TO_MINUTES = 60 * 1000;
+    public static final int MILLIS_TO_MINUTES = 60 * 1000;
 
 	/**
 	 * 获取当前分钟数
 	 * 
 	 * @return
 	 */
-	public static long getCurrentTimeOfMinutes() {
-		return System.currentTimeMillis() / MILLIS_TO_MINUTES;
+    public static long getCurrentTimeOfMinutes() {
+        return System.currentTimeMillis() / MILLIS_TO_MINUTES;
 	}
 
 }
